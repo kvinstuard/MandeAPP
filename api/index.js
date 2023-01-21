e3251faac518a9e50a5316361df9b602e3849451
juansebastian81 <juan.sebastian.ospina@correounivalle.edu.co>
2023-01-21 18:16:46 -0500

    Se crean carpetas y se modifica el api y el index

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(
+    express.urlencoded({
+        extended: true})
+)
+app.use(
+    express.json({
+        type: "*/*"
+    })
+)
+
+const server = app.listen(port, () =>{
+    let host = server.address().address
+    let port = server.address().port
+    console.log("Mande App listening at http://", host, port)
+} 
+)
