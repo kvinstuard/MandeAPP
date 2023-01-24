b9c5ee0aff23b22db9140bde17a0714027e13aee
juansebastian81 <juan.sebastian.ospina@correounivalle.edu.co>
2023-01-24 16:22:42 -0500

    se crea rutas, DB, controller y se modifica api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-const express = require('express')
+const express = require('express');
+const clientRoutes = require('../routes/index')
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(
     express.urlencoded({
@@ -14,8 +15,8 @@ app.use(
 )
 
 const server = app.listen(port, () =>{
-    let host = server.address().address
-    let port = server.address().port
+    let host = server.address().address;
+    let port = server.address().port;
     console.log("Mande App listening at http://", host, port)
 } 
 )
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const { Router } = requiere('express');
+
+const router = Router();
+
+module.exports = router;
