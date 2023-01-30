48ae3cbc58162a8a056a0256bad4e47793fe1e35
Kevin Stuard Marin C <kevin.concha@correounivalle.edu.co>
2023-01-30 17:54:19 -0500

    Delete routes directory

--- routes/queries.js
+++
@@ -1,7 +0,0 @@
-const getPersonas = 'SELECT * FROM persona';
-const getPersonaByID = 'SELECT * FROM persona WHERE ID = $1'; 
-
-module.exports = {
-    getPersonas,
-    getPersonaByID,
-}
--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const controller = require('./controllers');
-
-const router = Router();
-
-router.get('/', controller.getPersonas);
-router.get('/:ID', controller.getPersonaByID);
-
-module.exports = router;
