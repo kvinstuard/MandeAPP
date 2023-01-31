a14095739637a0f270312894469f7812145e3549
Kevin Stuard Marin C <kevin.concha@correounivalle.edu.co>
2023-01-31 01:43:53 -0500

    BD

---
+++ mandeApp.sql
@@ -0,0 +1,54 @@
+CREATE TABLE person(
+  name VARCHAR(90) NOT NULL,
+  last_name VARCHAR(90) NOT NULL,
+  address VARCHAR(90) NOT NULL,
+  password VARCHAR(20) NOT NULL,
+  coordinates VARCHAR(20)
+);
+
+CREATE TABLE usuario(
+	utility_bills BYTEA, 
+	phone_number VARCHAR(30) UNIQUE NOT NULL,
+	email VARCHAR(90) UNIQUE NOT NULL,
+	id VARCHAR(20) PRIMARY KEY NOT NULL
+) inherits (person);
+
+CREATE TABLE specialist(
+	bank_account VARCHAR(90) UNIQUE,
+	phone_number VARCHAR(30) UNIQUE NOT NULL,
+	email VARCHAR(90) UNIQUE NOT NULL,
+	id VARCHAR(20) PRIMARY KEY NOT NULL,
+	rate DECIMAL,
+	id_image BYTEA
+) inherits (person);
+
+CREATE TABLE task(
+	work_id SERIAL,  
+	hour_cost INTEGER NOT NULL,
+	name VARCHAR(90) NOT NULL,
+	bank_accountLB VARCHAR(90) REFERENCES specialist(bank_account)
+	ON DELETE RESTRICT
+);
+
+CREATE TABLE payment_method(
+	type_card VARCHAR(30) NOT NULL,
+	card_number VARCHAR(90) UNIQUE NOT NULL,
+	expiration_date DATE NOT NULL,
+	cvv INTEGER NOT NULL,
+	PRIMARY KEY(card_number, cvv),
+	phone_number VARCHAR(30) REFERENCES usuario (phone_number) ON DELETE RESTRICT	
+);
+
+
+CREATE TABLE services(
+	service_id VARCHAR(30) PRIMARY KEY NOT NULL,
+	bank_account VARCHAR(90) REFERENCES specialist(bank_account)
+	ON DELETE RESTRICT,
+	card_number_client VARCHAR(90) REFERENCES payment_method(card_number)
+	ON DELETE RESTRICT
+);
+
+DROP TABLE task CASCADE
+
+
+
